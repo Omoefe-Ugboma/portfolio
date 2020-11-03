7abe1d3d30d21364f89117406e36f98ef367d6f9
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2020-11-03 08:42:15 +0100

    work experience and clients

--- js/script.js
+++ js/script.js
@@ -51,3 +51,22 @@ galleryItem.forEach(function(element){
     })
 })
 
+
+// Testimonial
+const slides = document.querySelector(".slider").children;
+const indicatorImages = document.querySelector(".slider-indicator").children;
+
+for(let i=0; i<indicatorImages.length; i++){
+    indicatorImages[i].addEventListener("click",function(){
+        
+        for(let j=0; j<indicatorImages.length; j++){
+            indicatorImages[j].classList.remove("active");
+        }
+        this.classList.add("active");
+        const id=this.getAttribute("data-id");
+        for(let j=0; j<slides.length; j++){
+            slides[j].classList.remove("active");
+        }
+        slides[id].classList.add("active");
+    })
+}
